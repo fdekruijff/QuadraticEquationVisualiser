01162ad854947aa31ac22917f744228effd34cf3
Floris de Kruijff <floris.dekruijff@gmail.com>
2018-10-23 22:05:07 +0200

    Init upload QuadraticEquationVisualiser files

---
+++ QuadraticEquation.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace QuadraticEquation {
+    partial class QuadraticEquation {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.Canvas = new System.Windows.Forms.Panel();
+            this.ALabel = new System.Windows.Forms.Label();
+            this.BLabel = new System.Windows.Forms.Label();
+            this.CLabel = new System.Windows.Forms.Label();
+            this.AInput = new System.Windows.Forms.TextBox();
+            this.BInput = new System.Windows.Forms.TextBox();
+            this.CInput = new System.Windows.Forms.TextBox();
+            this.XMinLabel = new System.Windows.Forms.Label();
+            this.XMaxLabel = new System.Windows.Forms.Label();
+            this.YMinLabel = new System.Windows.Forms.Label();
+            this.YMaxLabel = new System.Windows.Forms.Label();
+            this.XMinInput = new System.Windows.Forms.TextBox();
+            this.XMaxInput = new System.Windows.Forms.TextBox();
+            this.YMinInput = new System.Windows.Forms.TextBox();
+            this.YMaxInput = new System.Windows.Forms.TextBox();
+            this.DiscriminantStaticLabel = new System.Windows.Forms.Label();
+            this.FormulaStaticLabel = new System.Windows.Forms.Label();
+            this.FormulaLabel = new System.Windows.Forms.Label();
+            this.DiscriminantLabel = new System.Windows.Forms.Label();
+            this.ZeroPointsStaticLabel = new System.Windows.Forms.Label();
+            this.ZeroPointsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Canvas
+            // 
+            this.Canvas.BackColor = System.Drawing.SystemColors.Control;
+            this.Canvas.Location = new System.Drawing.Point(277, 75);
+            this.Canvas.Name = "Canvas";
+            this.Canvas.Size = new System.Drawing.Size(819, 681);
+            this.Canvas.TabIndex = 0;
+            // 
+            // ALabel
+            // 
+            this.ALabel.AutoSize = true;
+            this.ALabel.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ALabel.Location = new System.Drawing.Point(25, 75);
+            this.ALabel.Name = "ALabel";
+            this.ALabel.Size = new System.Drawing.Size(36, 35);
+            this.ALabel.TabIndex = 1;
+            this.ALabel.Text = "a:";
+            // 
+            // BLabel
+            // 
+            this.BLabel.AutoSize = true;
+            this.BLabel.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BLabel.Location = new System.Drawing.Point(25, 110);
+            this.BLabel.Name = "BLabel";
+            this.BLabel.Size = new System.Drawing.Size(38, 35);
+            this.BLabel.TabIndex = 2;
+            this.BLabel.Text = "b:";
+            // 
+            // CLabel
+            // 
+            this.CLabel.AutoSize = true;
+            this.CLabel.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CLabel.Location = new System.Drawing.Point(25, 145);
+            this.CLabel.Name = "CLabel";
+            this.CLabel.Size = new System.Drawing.Size(35, 35);
+            this.CLabel.TabIndex = 3;
+            this.CLabel.Text = "c:";
+            // 
+            // AInput
+            // 
+            this.AInput.Location = new System.Drawing.Point(67, 85);
+            this.AInput.Name = "AInput";
+            this.AInput.Size = new System.Drawing.Size(176, 22);
+            this.AInput.TabIndex = 4;
+            // 
+            // BInput
+            // 
+            this.BInput.Location = new System.Drawing.Point(67, 120);
+            this.BInput.Name = "BInput";
+            this.BInput.Size = new System.Drawing.Size(176, 22);
+            this.BInput.TabIndex = 5;
+            // 
+            // CInput
+            // 
+            this.CInput.Location = new System.Drawing.Point(66, 155);
+            this.CInput.Name = "CInput";
+            this.CInput.Size = new System.Drawing.Size(176, 22);
+            this.CInput.TabIndex = 6;
+            // 
+            // XMinLabel
+            // 
+            this.XMinLabel.AutoSize = true;
+            this.XMinLabel.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.XMinLabel.Location = new System.Drawing.Point(25, 180);
+            this.XMinLabel.Name = "XMinLabel";
+            this.XMinLabel.Size = new System.Drawing.Size(86, 35);
+            this.XMinLabel.TabIndex = 7;
+            this.XMinLabel.Text = "x-min:";
+            // 
+            // XMaxLabel
+            // 
+            this.XMaxLabel.AutoSize = true;
+            this.XMaxLabel.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.XMaxLabel.Location = new System.Drawing.Point(25, 215);
+            this.XMaxLabel.Name = "XMaxLabel";
+            this.XMaxLabel.Size = new System.Drawing.Size(90, 35);
+            this.XMaxLabel.TabIndex = 8;
+            this.XMaxLabel.Text = "x-max:";
+            // 
+            // YMinLabel
+            // 
+            this.YMinLabel.AutoSize = true;
+            this.YMinLabel.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.YMinLabel.Location = new System.Drawing.Point(25, 250);
+            this.YMinLabel.Name = "YMinLabel";
+            this.YMinLabel.Size = new System.Drawing.Size(87, 35);
+            this.YMinLabel.TabIndex = 9;
+            this.YMinLabel.Text = "y-min:";
+            // 
+            // YMaxLabel
+            // 
+            this.YMaxLabel.AutoSize = true;
+            this.YMaxLabel.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.YMaxLabel.Location = new System.Drawing.Point(25, 285);
+            this.YMaxLabel.Name = "YMaxLabel";
+            this.YMaxLabel.Size = new System.Drawing.Size(91, 35);
+            this.YMaxLabel.TabIndex = 10;
+            this.YMaxLabel.Text = "y-max:";
+            // 
+            // XMinInput
+            // 
+            this.XMinInput.Location = new System.Drawing.Point(116, 190);
+            this.XMinInput.Name = "XMinInput";
+            this.XMinInput.Size = new System.Drawing.Size(126, 22);
+            this.XMinInput.TabIndex = 11;
+            // 
+            // XMaxInput
+            // 
+            this.XMaxInput.Location = new System.Drawing.Point(116, 225);
+            this.XMaxInput.Name = "XMaxInput";
+            this.XMaxInput.Size = new System.Drawing.Size(126, 22);
+            this.XMaxInput.TabIndex = 12;
+            // 
+            // YMinInput
+            // 
+            this.YMinInput.Location = new System.Drawing.Point(116, 260);
+            this.YMinInput.Name = "YMinInput";
+            this.YMinInput.Size = new System.Drawing.Size(126, 22);
+            this.YMinInput.TabIndex = 13;
+            // 
+            // YMaxInput
+            // 
+            this.YMaxInput.Location = new System.Drawing.Point(116, 295);
+            this.YMaxInput.Name = "YMaxInput";
+            this.YMaxInput.Size = new System.Drawing.Size(126, 22);
+            this.YMaxInput.TabIndex = 14;
+            // 
+            // DiscriminantStaticLabel
+            // 
+            this.DiscriminantStaticLabel.AutoSize = true;
+            this.DiscriminantStaticLabel.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DiscriminantStaticLabel.Location = new System.Drawing.Point(25, 439);
+            this.DiscriminantStaticLabel.Name = "DiscriminantStaticLabel";
+            this.DiscriminantStaticLabel.Size = new System.Drawing.Size(165, 35);
+            this.DiscriminantStaticLabel.TabIndex = 17;
+            this.DiscriminantStaticLabel.Text = "Discriminant:";
+            // 
+            // FormulaStaticLabel
+            // 
+            this.FormulaStaticLabel.AutoSize = true;
+            this.FormulaStaticLabel.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormulaStaticLabel.Location = new System.Drawing.Point(25, 346);
+            this.FormulaStaticLabel.Name = "FormulaStaticLabel";
+            this.FormulaStaticLabel.Size = new System.Drawing.Size(117, 35);
+            this.FormulaStaticLabel.TabIndex = 18;
+            this.FormulaStaticLabel.Text = "Formula:";
+            // 
+            // FormulaLabel
+            // 
+            this.FormulaLabel.AutoSize = true;
+            this.FormulaLabel.Font = new System.Drawing.Font("Calibri Light", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormulaLabel.Location = new System.Drawing.Point(25, 381);
+            this.FormulaLabel.Name = "FormulaLabel";
+            this.FormulaLabel.Size = new System.Drawing.Size(155, 29);
+            this.FormulaLabel.TabIndex = 19;
+            this.FormulaLabel.Text = "Lorem Impsum";
+            // 
+            // DiscriminantLabel
+            // 
+            this.DiscriminantLabel.AutoSize = true;
+            this.DiscriminantLabel.Font = new System.Drawing.Font("Calibri Light", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DiscriminantLabel.Location = new System.Drawing.Point(26, 474);
+            this.DiscriminantLabel.Name = "DiscriminantLabel";
+            this.DiscriminantLabel.Size = new System.Drawing.Size(136, 29);
+            this.DiscriminantLabel.TabIndex = 20;
+            this.DiscriminantLabel.Text = "Lorem Ipsum";
+            // 
+            // ZeroPointsStaticLabel
+            // 
+            this.ZeroPointsStaticLabel.AutoSize = true;
+            this.ZeroPointsStaticLabel.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ZeroPointsStaticLabel.Location = new System.Drawing.Point(24, 532);
+            this.ZeroPointsStaticLabel.Name = "ZeroPointsStaticLabel";
+            this.ZeroPointsStaticLabel.Size = new System.Drawing.Size(100, 35);
+            this.ZeroPointsStaticLabel.TabIndex = 21;
+            this.ZeroPointsStaticLabel.Text = "f(x) = 0:";
+            // 
+            // ZeroPointsLabel
+            // 
+            this.ZeroPointsLabel.AutoSize = true;
+            this.ZeroPointsLabel.Font = new System.Drawing.Font("Calibri Light", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ZeroPointsLabel.Location = new System.Drawing.Point(26, 567);
+            this.ZeroPointsLabel.Name = "ZeroPointsLabel";
+            this.ZeroPointsLabel.Size = new System.Drawing.Size(136, 29);
+            this.ZeroPointsLabel.TabIndex = 22;
+            this.ZeroPointsLabel.Text = "Lorem Ipsum";
+            // 
+            // QuadraticEquation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(1108, 768);
+            this.Controls.Add(this.ZeroPointsLabel);
+            this.Controls.Add(this.ZeroPointsStaticLabel);
+            this.Controls.Add(this.DiscriminantLabel);
+            this.Controls.Add(this.FormulaLabel);
+            this.Controls.Add(this.FormulaStaticLabel);
+            this.Controls.Add(this.DiscriminantStaticLabel);
+            this.Controls.Add(this.YMaxInput);
+            this.Controls.Add(this.YMinInput);
+            this.Controls.Add(this.XMaxInput);
+            this.Controls.Add(this.XMinInput);
+            this.Controls.Add(this.YMaxLabel);
+            this.Controls.Add(this.YMinLabel);
+            this.Controls.Add(this.XMaxLabel);
+            this.Controls.Add(this.XMinLabel);
+            this.Controls.Add(this.CInput);
+            this.Controls.Add(this.BInput);
+            this.Controls.Add(this.AInput);
+            this.Controls.Add(this.CLabel);
+            this.Controls.Add(this.BLabel);
+            this.Controls.Add(this.ALabel);
+            this.Controls.Add(this.Canvas);
+            this.Name = "QuadraticEquation";
+            this.Text = "Quadratic Equation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Canvas;
+        private System.Windows.Forms.Label ALabel;
+        private System.Windows.Forms.Label BLabel;
+        private System.Windows.Forms.Label CLabel;
+        private System.Windows.Forms.TextBox AInput;
+        private System.Windows.Forms.TextBox BInput;
+        private System.Windows.Forms.TextBox CInput;
+        private System.Windows.Forms.Label XMinLabel;
+        private System.Windows.Forms.Label XMaxLabel;
+        private System.Windows.Forms.Label YMinLabel;
+        private System.Windows.Forms.Label YMaxLabel;
+        private System.Windows.Forms.TextBox XMinInput;
+        private System.Windows.Forms.TextBox XMaxInput;
+        private System.Windows.Forms.TextBox YMinInput;
+        private System.Windows.Forms.TextBox YMaxInput;
+        private System.Windows.Forms.Label DiscriminantStaticLabel;
+        private System.Windows.Forms.Label FormulaStaticLabel;
+        private System.Windows.Forms.Label FormulaLabel;
+        private System.Windows.Forms.Label DiscriminantLabel;
+        private System.Windows.Forms.Label ZeroPointsStaticLabel;
+        private System.Windows.Forms.Label ZeroPointsLabel;
+    }
+}
+
---
+++ QuadraticEquation.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace QuadraticEquation {
+    public partial class QuadraticEquation : Form {
+        private double A, B, C, xMin, xMax, yMin, yMax, Scale;
+        private Point MouseDownLocation;
+        private Point Offset;
+        private Point Cells;
+        private Point Step;
+
+        public QuadraticEquation() {
+            InitializeComponent();
+            InitGraphValue();
+
+            Scale = 1;
+
+            Canvas.Paint += Draw;
+            Canvas.MouseClick += Zoom;
+            Canvas.MouseMove += Drag;
+            Canvas.MouseDown += Down;
+            XMinInput.TextChanged += Update;
+            YMinInput.TextChanged += Update;
+            XMaxInput.TextChanged += Update;
+            YMaxInput.TextChanged += Update;
+            AInput.TextChanged += Update;
+            BInput.TextChanged += Update;
+            CInput.TextChanged += Update;
+        }
+
+        private void InitGraphValue() {
+            SetA(-2); SetB(2); SetC(4);
+            SetXMin(-3); SetXMax(3);
+            SetYMin(-2); SetYMax(5);
+            FormulaLabel.Text = FormulateFormula();
+        }
+
+        private void Update(object o, EventArgs ea) {
+            try {
+                SetXMin(Convert.ToDouble(XMinInput.Text));
+                SetYMin(Convert.ToDouble(YMinInput.Text));
+                SetXMax(Convert.ToDouble(XMaxInput.Text));
+                SetYMax(Convert.ToDouble(YMaxInput.Text));
+                SetA(Convert.ToDouble(AInput.Text));
+                SetB(Convert.ToDouble(BInput.Text));
+                SetC(Convert.ToDouble(CInput.Text));
+
+                FormulaLabel.Text = FormulateFormula();
+                Canvas.Invalidate();
+            } catch (Exception e) { }
+        }
+
+        private string FormulateFormula() {
+            string Formula = "f(x) = ";
+
+            if (A != 0) {
+                if (A == 1) Formula += "x\xB2";
+                else if (A == -1) Formula += "-x\xB2";
+                else Formula += A + "x\xB2";
+            }
+
+            if (B != 0) {
+                if (B == 1) Formula += " + x";
+                else if (B == -1) Formula += " - x";
+                else if (B < 0) Formula += " - " + Math.Abs(B) + "x";
+                else Formula += " + " + B + "x";
+            }
+
+            if (C != 0) {
+                if (C > 0) Formula += " + " + C;
+                else Formula += " - " + Math.Abs(C);
+            }
+
+
+
+            return Formula;
+        }
+
+        private void DrawFormula(Graphics gr) {
+            Pen GraphPoint = new Pen(Brushes.Red, 2);
+            Point[] Points = new Point[Canvas.Width];
+
+            for (int i = 0; i <= Canvas.Width - 1; i++) {
+                double x = (-(Canvas.Width / 2) + i) / Convert.ToDouble(Canvas.Width) * Cells.X;
+                double y = (A * Math.Pow(x, 2) + (B * x) + C);
+
+                double relx = ((int)(Math.Abs(xMin) * Step.X) + (x / Cells.X * Canvas.Width)) + Offset.X;
+                double rely = ((int)(Math.Abs(yMax) * Step.Y) - (y / Cells.Y * Canvas.Height)) + Offset.Y;
+
+                Points[i] = new Point(Convert.ToInt32(relx - 1), Convert.ToInt32(rely - 1));
+            }
+
+            gr.DrawLines(GraphPoint, Points);
+        }
+
+        private void DrawAxis(Graphics gr) {
+            Pen ZeroAxis = new Pen(Color.Blue, 3);
+            Pen Axis = new Pen(Color.Gray, 1);
+
+            // Draw zero axis
+            if (xMin <= 0 && xMax >= 0)
+                gr.DrawLine(
+                    pen: ZeroAxis,
+                    x1: (int)(Math.Abs(xMin) * Step.X + Offset.X),
+                    y1: 0,
+                    x2: (int)(Math.Abs(xMin) * Step.X + Offset.X),
+                    y2: Canvas.Height
+                );
+            if (yMin <= 0 && yMax >= 0)
+                gr.DrawLine(
+                    pen: ZeroAxis,
+                    x1: 0,
+                    y1: (int)(Math.Abs(yMax) * Step.Y + Offset.Y),
+                    x2: Canvas.Width,
+                    y2: (int)(Math.Abs(yMax) * Step.Y + Offset.Y)
+                );
+
+            // Draw X-Axis
+            for (int i = 0; i <= Cells.X; i++) {
+                gr.DrawLine(
+                    pen: Axis,
+                    x1: (int)(Canvas.Width / Cells.X * i + (Offset.X % Step.X)),
+                    y1: 0,
+                    x2: (int)(Canvas.Width / Cells.X * i + (Offset.X % Step.X)),
+                    y2: Canvas.Height
+                );
+            }
+
+            // Draw Y-Axis
+            for (int i = 0; i <= Cells.Y; i++) {
+                gr.DrawLine(
+                    pen: Axis,
+                    x1: 0,
+                    y1: (int)(Canvas.Height / Cells.Y * i + (Offset.Y % Step.Y)),
+                    x2: Canvas.Width,
+                    y2: (int)(Canvas.Height / Cells.Y * i + (Offset.Y % Step.Y))
+                );
+            }
+
+            // Attach coordinate labels
+            Font LabelFont = new Font("Calibri", 7);
+            for (int x = 0; x <= Cells.X; x++) {
+                for (int y = 0; y <= Cells.Y; y++) {
+                    int dx = (int)(Offset.X / Step.X);
+                    int dy = (int)(Offset.Y / Step.Y);
+                    string LabelText = "(" + (xMin + x - dx).ToString() + "," + (yMax - y + dy) + ")";
+                    gr.DrawString(LabelText, LabelFont, Brushes.Gray, (int)(Canvas.Width / Cells.X * x + Offset.X % Step.X), (int)(Canvas.Height / Cells.Y * y + Offset.Y % Step.Y));
+                }
+            }
+
+        }
+
+        private void Down(object o, MouseEventArgs mea) {
+            if (mea.Button == MouseButtons.Left) {
+                MouseDownLocation = mea.Location;
+            }
+        }
+
+        private void Drag(object o, MouseEventArgs mea) {
+            if (mea.Button == MouseButtons.Left) {
+                Offset.X = Offset.X + (mea.X - MouseDownLocation.X) / 10;
+                Offset.Y = Offset.Y + (mea.Y - MouseDownLocation.Y) / 10;
+                Canvas.Invalidate();
+            }
+
+        }
+
+        private void CalculateFormula() {
+            double Delta = Math.Pow(B, 2) - (4 * A * C);
+            DiscriminantLabel.Text = Delta.ToString();
+
+            if (Delta > 0) {
+                // 2 Solutions
+                double X1 = Math.Round((-B + Math.Sqrt(Delta)) / (2 * A), 2);
+                double X2 = Math.Round((-B - Math.Sqrt(Delta)) / (2 * A), 2);
+
+                ZeroPointsLabel.Text = "x\u2081 = " + X1 + " \u2228 x\u2082 = " + X2;
+            } else if (Delta == 0) {
+                // 1 Solution
+                double X1 = (-B + Math.Sqrt(Delta)) / 2 * A;
+                ZeroPointsLabel.Text = "x\u2081 = " + X1;
+            } else {
+                // No solutions
+                ZeroPointsLabel.Text = "None.";
+            }
+
+        }
+
+        private void Zoom(object o, MouseEventArgs mea) {
+            if (mea.Button == MouseButtons.Right) {
+                //Scale *= 2;
+            } else if (mea.Button == MouseButtons.Left) {
+                //Scale *= 0.5;
+            }
+            Canvas.Invalidate();
+        }
+
+        private void Draw(object o, PaintEventArgs pea) {
+            Cells.X = Convert.ToInt32(Math.Abs(xMin) + Math.Abs(xMax) * Scale);
+            Cells.Y = Convert.ToInt32(Math.Abs(yMin) + Math.Abs(yMax) * Scale);
+            Step.X = Canvas.Width / Cells.X;
+            Step.Y = Canvas.Height / Cells.Y;
+
+            DrawAxis(pea.Graphics);
+            DrawFormula(pea.Graphics);
+            CalculateFormula();
+        }
+
+        private void SetA(double value) {
+            A = value;
+            AInput.Text = value.ToString();
+        }
+
+        private void SetB(double value) {
+            B = value;
+            BInput.Text = value.ToString();
+        }
+
+        private void SetC(double value) {
+            C = value;
+            CInput.Text = value.ToString();
+        }
+
+        private void SetXMin(double value) {
+            xMin = value;
+            XMinInput.Text = value.ToString();
+        }
+
+        private void SetXMax(double value) {
+            xMax = value;
+            XMaxInput.Text = value.ToString();
+        }
+
+        private void SetYMin(double value) {
+            yMin = value;
+            YMinInput.Text = value.ToString();
+        }
+
+        private void SetYMax(double value) {
+            yMax = value;
+            YMaxInput.Text = value.ToString();
+        }
+    }
+}
